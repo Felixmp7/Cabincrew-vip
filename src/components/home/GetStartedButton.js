442a3941613dcc3de6438ace9b27480420469074
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-03-11 11:53:51 -0400

    Arreglando Bug en los estilos del Button.sass

--- src/components/home/GetStartedButton.js
+++ src/components/home/GetStartedButton.js
@@ -12,20 +12,4 @@ const GetStartedButton = () => {
 }
 
 export default GetStartedButton;
-
-// .cont-button
-// height: 50px
-// .get-started
-// 	width: 100%
-// 	height: 100%
-// 	background-color: rgba(255,255,255,0)
-// 	border: 2px solid #5E0F15
-// 	border-radius: 3px
-// 	cursor: pointer
-// 	color: #5E0F15
-// 	font-size: 18px
-// 	transition: all .6s ease
-// 	&:hover
-// 		background-color: rgba(94, 15, 21, 1)
-// 		color: white
-// 		transition: all .6s ease
+	
