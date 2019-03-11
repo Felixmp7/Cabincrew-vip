3dfd728938336510d29451fe976e755d9764d935
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-03-11 11:11:18 -0400

    Creando componente de Boton GetStarted

---
+++ src/components/home/GetStartedButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './../../styles/home/title/Title.css';
+
+const GetStartedButton = () => {
+	return (
+		<div className="positionbutton">
+			<div className="cont-button">
+				<button className="get-started">Get Started</button>
+			</div>
+		</div>
+	);
+}
+
+export default GetStartedButton;
