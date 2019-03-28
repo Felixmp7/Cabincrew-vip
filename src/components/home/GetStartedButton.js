f3091f299c0c162639ace0f0945b8611a3d40246
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-03-28 14:24:05 -0400

    Puliendo posiciones del boton en home

--- src/components/home/GetStartedButton.js
+++ src/components/home/GetStartedButton.js
@@ -3,13 +3,10 @@ import './../../styles/home/GetStarted/Button.css'
 
 const GetStartedButton = () => {
 	return (
-		<div className="position-button">
 			<div className="cont-button">
 				<button className="get-started">Get Started</button>
 			</div>
-		</div>
 	);
 }
 
 export default GetStartedButton;
-	
