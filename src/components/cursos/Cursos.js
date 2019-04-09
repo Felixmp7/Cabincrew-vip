2d3b244899cd9abebaba7eb03392097f6ff7f416
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-04-09 17:49:20 -0400

    Acomodando contenido de los cursos

--- src/components/cursos/Cursos.js
+++ src/components/cursos/Cursos.js
@@ -9,7 +9,7 @@ const Cursos = () => (
     </div>
     <div className="info">
       <h2>Te ofrecemos cursos</h2>
-      <h4>Para la formacion de profesionales y técnicos del medio Aeronáutico</h4>
+      <h4>Para la formación de profesionales y técnicos del medio Aeronáutico.</h4>
       <p>
         Te brindaremos una guía direccional, adaptable a las circunstancias del momento,
         para alcanzar los fines y objetivos de le Empresa.
