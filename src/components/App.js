314524acfb11ded0d3e8fcb6930715be1919d555
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-04-14 13:01:05 -0400

    Renderizando todo el home y comentando tareas pendientes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import '../styles/app/App.css';
-// import Home from './home/Home';
+import Home from './home/Home';
 import Nosotros from './nosotros/Nosotros'
-// import Cursos from './cursos/Cursos'
-// import Banner from './banner/Banner'
-// import Equipo from './equipo/Equipo'
-// import Footer from './footer/Footer'
+import Cursos from './cursos/Cursos'
+import Banner from './banner/Banner'
+import Equipo from './equipo/Equipo'
+import Footer from './footer/Footer'
 //Media
 import p1 from '../media/1-Persona.png'
 import p2 from '../media/2-Persona.png'
@@ -34,12 +34,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Home /> */}
-        <Nosotros />
-        {/* <Cursos/> */}
-        {/* <Banner/> */}
-        {/* <Equipo data={this.state.teachers}/> */}
-        {/* <Footer/> */}
+        <Home />
+        <Nosotros /> {/*Posicionar tarjetas en columna*/}
+        <Cursos/>
+        <Banner/>
+        <Equipo data={this.state.teachers}/>
+        <Footer/> {/* arreglar tamaño del footer en desktop*/}
       </div>
     );
   }
