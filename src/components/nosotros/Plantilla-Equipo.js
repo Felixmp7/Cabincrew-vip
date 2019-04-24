4856b2ba93c90cf855b64d667207a02371f77a77
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-04-24 11:41:38 -0400

    Reutilizando codigo de Material-UI

--- src/components/nosotros/Plantilla-Equipo.js
+++ src/components/nosotros/Plantilla-Equipo.js
@@ -1,52 +1,35 @@
-// Dependencies
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/styles';
 import classnames from 'classnames';
 import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import red from '@material-ui/core/colors/red';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-// PropTypes
-import PropTypes from 'prop-types';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
   card: {
     maxWidth: 400,
-    margin: 'auto',
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
   },
   actions: {
     display: 'flex',
-    backgroundColor: 'white',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  cardHeader: {
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    padding: 5
-  },
-  iconContainer: {
-    width: 40,
-    paddingRight: 10,
-    marginRight: 10,
-  },
-  Img: {
-    width: '100%'
-  },
-  cardTitle: {
-    fontSize: 22,
-    fontWeight: 'lighter',
-  },
-  contenido: {
-    backgroundColor: '#f0f0f0',
   },
   expand: {
     transform: 'rotate(0deg)',
+    marginLeft: 'auto',
     transition: theme.transitions.create('transform', {
       duration: theme.transitions.duration.shortest,
     }),
@@ -54,68 +37,93 @@ const styles = theme => ({
   expandOpen: {
     transform: 'rotate(180deg)',
   },
-});
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
 
-class PlantillaTarjeta extends React.Component {
-  state = { expanded: false };
+function RecipeReviewCard() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
 
-  handleExpandClick = () => {
-    this.setState(state => ({ expanded: !state.expanded }));
-  };
-
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <Card className={classes.card}>
-        <CardActions className={classes.actions} disableActionSpacing>
-          <CardContent style={{width: '80%', padding: 5}}>
-            <div className={classes.cardHeader}>
-              <div className={classes.iconContainer} style={{borderRight: `${this.props.border} ${this.props.color}`}}>
-                <img className={classes.Img} src={this.props.image} alt={this.props.title}/>
-              </div>
-              <span className={classes.cardTitle} style={{color: this.props.color}}>
-                {this.props.title}
-              </span>
-            </div>
-          </CardContent>
-
-          <IconButton
-            className={classnames(classes.expand, {
-              [classes.expandOpen]: this.state.expanded,
-            })}
-            onClick={this.handleExpandClick}
-            aria-expanded={this.state.expanded}
-            aria-label="Show more"
-          >
-            <ExpandMoreIcon />
-          </IconButton>
-        </CardActions>
-        <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
-          <CardContent className={classes.contenido}>
-            <Typography paragraph>Los objetivos que se ha planteado la Empresa para el
-            corto y mediano plazo son los siguientes:</Typography>
-            <Typography paragraph>
-              Desarrollar cursos para la formación de Técnicos Aeronáuticos en las especialidades más
-              importantes y que se consideren de mayor necesidad para el desarrollo de la Industria
-              Aeronáutica Nacional e Internacional.
-
-              Mantenerse como empresa líder del país en la Formación de Tripulantes de Cabina y de
-              Instructores Aeronáuticos.
-              Estructurar cursos de mejoramiento profesional en diferentes áreas y que en forma
-              periódica puedan ser ofrecidos a todas las empresas de aviación que participan en
-              la actividad diaria del país y del mundo.
-              Garantizar el mejor aprovechamiento de los recursos con los cuales cuenta.
-            </Typography>
-          </CardContent>
-        </Collapse>
-      </Card>
-    );
+  function handleExpandClick() {
+    setExpanded(!expanded);
   }
+
+  return (
+    <Card className={classes.card}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="Recipe" className={classes.avatar}>
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+      <CardMedia
+        className={classes.media}
+        image="/static/images/cards/paella.jpg"
+        title="Paella dish"
+      />
+      <CardContent>
+        <Typography component="p">
+          This impressive paella is a perfect party dish and a fun meal to cook together with your
+          guests. Add 1 cup of frozen peas along with the mussels, if you like.
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.actions} disableActionSpacing>
+        <IconButton aria-label="Add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton aria-label="Share">
+          <ShareIcon />
+        </IconButton>
+        <IconButton
+          className={classnames(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="Show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Method:</Typography>
+          <Typography paragraph>
+            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+            minutes.
+          </Typography>
+          <Typography paragraph>
+            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
+            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
+            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
+            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+          </Typography>
+          <Typography paragraph>
+            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
+            again without stirring, until mussels have opened and rice is just tender, 5 to 7
+            minutes more. (Discard any mussels that don’t open.)
+          </Typography>
+          <Typography>
+            Set aside off of the heat to let rest for 10 minutes, and then serve.
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
 }
 
-PlantillaTarjeta.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(PlantillaTarjeta);
+export default RecipeReviewCard;
