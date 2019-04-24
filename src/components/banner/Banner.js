0143e761be87490fd4801426e44c6c4a3ac8a2b9
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-04-24 11:08:41 -0400

    Corrigiendo className dinamico

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -3,12 +3,8 @@ import '../../styles/banner/Banner.css'
 
 const Banner = props => (
   <div className="Banner">
-    <div className="container">
-      {
-        props.height?
-        <h2 className={props.classNameDynamic}>{props.title}</h2>:
-        <h2>{props.title}</h2>
-      }
+    <div className={`container ${props.classNameDynamic}`}>
+      <h2>{props.title}</h2>
       <p>{props.contain}</p>
       {
         props.titleButton ?
