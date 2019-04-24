3768dd79e3b26eb9bb8d33173c5cdad8d1d5c365
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-04-24 13:32:38 -0400

    Añadiendo if al contenido

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -5,7 +5,7 @@ const Banner = props => (
   <div className={`Banner ${props.classNameDynamic}`}>
     <div className="container">
       <h2>{props.title}</h2>
-      <p>{props.contain}</p>
+      { props.contain && <p>{props.contain}</p>}
       {
         props.titleButton &&
         <div className="cont-Button">
