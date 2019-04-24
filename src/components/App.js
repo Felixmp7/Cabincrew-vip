06b1422e503ade944386e1ff6c1aad45dc893989
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-04-24 10:17:39 -0400

    Sacando los enlaces de la clase App

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,23 @@ import React, { Component } from 'react';
 import '../styles/app/App.css';
 import Navbar from './navbar/Navbar'
 //import Home from './home/Home'
+import Nosotros from './nosotros/Nosotros'
+
+const enlaces = [
+  'Home',
+  'Nosotros',
+  'Servicios',
+  'Galeria',
+  'Contacto',
+]
 
 class App extends Component {
-  state = [
-  	'Home',
-  	'Nosotros',
-  	'Servicios',
-  	'Galeria',
-  	'Contacto',
-  ]
   render() {
     return (
       <div className="App">
-  			<Navbar enlaces={this.state} />
+  			<Navbar enlaces={enlaces} />
         {/* <Home/> */}
+        <Nosotros/>
       </div>
     );
   }
