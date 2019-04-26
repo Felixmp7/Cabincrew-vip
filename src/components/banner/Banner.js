75e9eff3be71587d08297d90ad519fd4129797b6
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-04-26 09:07:50 -0400

    Ignore

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -5,7 +5,9 @@ const Banner = props => (
   <div className={`Banner ${props.classNameDynamic}`}>
     <div className="container">
       <h2>{props.title}</h2>
-      { props.contain && <p>{props.contain}</p>}
+      {
+        props.contain && <p>{props.contain}</p>
+      }
       {
         props.titleButton &&
         <div className="cont-Button">
