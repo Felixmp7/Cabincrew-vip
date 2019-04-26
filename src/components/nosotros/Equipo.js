0fa908bd9fe9de945d87d3b30bded7e4e8309df3
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-04-26 11:39:18 -0400

    Añadiendo titulo del equipo

--- src/components/nosotros/Equipo.js
+++ src/components/nosotros/Equipo.js
@@ -7,61 +7,17 @@ import p1 from '../../media/1-Persona.png'
 import p2 from '../../media/2-Persona.png'
 import p3 from '../../media/3-Persona.png'
 
-const data = [
-  {
-    name: 'Lic. Diana Gomez',
-    ocupation: 'Directora',
-    img: p1,
-    materias:[
-      'Apariencia Personal','Servicio a Bordo','Obligaciones y Responsabilidades',
-    ],
-  },
-  {
-    name: 'Cap. Martin Borges',
-    ocupation: 'Director Ejecutivo',
-    img: p2,
-    materias:[
-      'Apariencia Personal','Servicio a Bordo','Obligaciones y Responsabilidades',
-    ],
-  },
-  {
-    name: 'Dra. Ana Yolanda',
-    ocupation: 'Directora de SMS',
-    img: p3,
-    materias:[
-      'Apariencia Personal','Servicio a Bordo','Obligaciones y Responsabilidades',
-    ],
-  },
-  {
-    name: 'Lic. Diana Gomez',
-    ocupation: 'Directora',
-    img: p1,
-    materias:[
-      'Apariencia Personal','Servicio a Bordo','Obligaciones y Responsabilidades',
-    ],
-  },
-  {
-    name: 'Cap. Martin Borges',
-    ocupation: 'Director Ejecutivo',
-    img: p2,
-    materias:[
-      'Apariencia Personal','Servicio a Bordo','Obligaciones y Responsabilidades',
-    ],
-  },
-  {
-    name: 'Dra. Ana Yolanda',
-    ocupation: 'Directora de SMS',
-    img: p3,
-    materias:[
-      'Apariencia Personal','Servicio a Bordo','Obligaciones y Responsabilidades',
-    ],
-  },
-]
-
 class Equipo extends React.Component {
   render() {
     return (
       <div className="Equipo">
+        <div className="Title-Container">
+          <h2 className="Title">Nuestro equipo profesional</h2>
+          <p>
+            Contamos con un equipo de gran calidad humana, servicial y dispuestos
+            a brindarte ayuda en todo lo que necesites.
+          </p>
+        </div>
         <div className="Teachers-Container">
           {
             data.map((teacher,index) =>{
@@ -82,5 +38,55 @@ class Equipo extends React.Component {
     )
   }
 }
+const data = [
+  {
+    name: 'Lic. Diana Gomez',
+    ocupation: 'Directora',
+    img: p3,
+    materias:[
+      'Apariencia Personal','Servicio a Bordo','Obligaciones y Responsabilidades',
+    ],
+  },
+  {
+    name: 'Cap. Martin Borges',
+    ocupation: 'Director Ejecutivo',
+    img: p1,
+    materias:[
+      'Apariencia Personal','Servicio a Bordo','Obligaciones y Responsabilidades',
+    ],
+  },
+  {
+    name: 'Dra. Ana Yolanda',
+    ocupation: 'Directora de SMS',
+    img: p2,
+    materias:[
+      'Apariencia Personal','Servicio a Bordo','Obligaciones y Responsabilidades',
+    ],
+  },
+  {
+    name: 'Lic. Diana Gomez',
+    ocupation: 'Directora',
+    img: p3,
+    materias:[
+      'Apariencia Personal','Servicio a Bordo','Obligaciones y Responsabilidades',
+    ],
+  },
+  {
+    name: 'Cap. Martin Borges',
+    ocupation: 'Director Ejecutivo',
+    img: p1,
+    materias:[
+      'Apariencia Personal','Servicio a Bordo','Obligaciones y Responsabilidades',
+    ],
+  },
+  {
+    name: 'Dra. Ana Yolanda',
+    ocupation: 'Directora de SMS',
+    img: p2,
+    materias:[
+      'Apariencia Personal','Servicio a Bordo','Obligaciones y Responsabilidades',
+    ],
+  },
+]
 
 export default Equipo
