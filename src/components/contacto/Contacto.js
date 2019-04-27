e573f89947f7cc132f5a3068ca81eddc20e49f8d
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-04-27 13:05:45 -0400

    Creando Seccion superior de Contacto

--- src/components/contacto/Contacto.js
+++ src/components/contacto/Contacto.js
@@ -2,7 +2,31 @@ import React from 'react'
 // import '../../styles/contacto/Contacto.css'
 
 const Contacto = () => (
-  <div>Hola</div>
+  <div className="Contacto">
+    <div className="Contacto-Header">
+      <div className="Title">
+        <h3>¿Cómo te podemos ayudar?</h3>
+      </div>
+      <div className="Subtitle">
+        <p>
+          Rellena el siguiente formulario para brindarte
+          la mejor solucion. <br/>
+          Estamos aquí para ti.
+        </p>
+      </div>
+      <div className="Form">
+        <form>
+          <input type="text" placeholder="Nombre"/>
+          <input type="text" placeholder="Apellido" />
+          <input type="email" placeholder="Correo Electrónico" />
+          <input type="text" placeholder="Numero de Teléfono" />
+          <input type="text" placeholder="Escribe tu solicitud" />
+        </form>
+      </div>
+    </div>
+    <div className="Contacto-Info"></div>
+    <div className="Contacto-Footer"></div>
+  </div>
 )
 
 export default Contacto
