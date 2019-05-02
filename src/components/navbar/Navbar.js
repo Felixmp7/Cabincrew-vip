ffe6cfc72956c0eb07fdcdd9acda39ce39068eb1
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-02 15:03:58 -0400

    Quitando bordes del menu de forma dinamica

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -10,48 +10,52 @@ import BurgerMenu from './navbar-mobile/BurgerMenu'
 
 class Navbar extends Component {
 	setRef = element => {
-		this.menu = element
-		// console.log(this.menu)
+		if (element.className === 'menu') {
+			this.menu = element
+			console.log(element.className)
+		}
+
+		if (element.className === 'nav-cont') {
+			this.nav = element
+			console.log(element.className)
+		}
 	}
 	setBackgroundColor = event =>{
 		this.menu.classList.add('background-dynamic')
+		this.nav.classList.add('border-dynamic')
 	}
 
 	setNormalBackgroundColor = event =>{
 		this.menu.classList.remove('background-dynamic')
+		this.nav.classList.remove('border-dynamic')
 	}
 	render(){
 		if (isWidthUp('md', this.props.width)) {
 			return (
 				<div className="nav-position">
-					<div className="nav-cont">
+					<div className="nav-cont" ref={this.setRef}>
 						<nav className="menu" ref={this.setRef}>
 							<ul className="lista">
-							{/* {
-								this.props.enlaces.map((enlace, index) =>{
-									if (index === 0) {
-										return(
-											<Link
-												to="/"
-												key={index}
-												>{enlace}</Link>
-											)
-									}
-									else {
-										return(
-											<Link
-												to={`/${enlace.toLowerCase()}`}
-												key={index}
-												>{enlace}</Link>
-											)
-									}
-								})
-							} */}
-							<Link to="/" onClick={this.setNormalBackgroundColor}>Home</Link>
-							<Link to="/nosotros" onClick={this.setBackgroundColor}>Nosotros</Link>
-							<Link to="/servicios" onClick={this.setNormalBackgroundColor}>Servicios</Link>
-							<Link to="/galeria" onClick={this.setNormalBackgroundColor}>Galeria</Link>
-							<Link to="/contacto" onClick={this.setBackgroundColor}>Contacto</Link>
+							<Link
+								to="/"
+								onClick={this.setNormalBackgroundColor}
+							>Home</Link>
+							<Link
+								to="/nosotros"
+								onClick={this.setBackgroundColor}
+							>Nosotros</Link>
+							<Link
+								to="/servicios"
+								onClick={this.setNormalBackgroundColor}
+							>Servicios</Link>
+							<Link
+								to="/galeria"
+								onClick={this.setNormalBackgroundColor}
+							>Galeria</Link>
+							<Link
+								to="/contacto"
+								onClick={this.setBackgroundColor}
+							>Contacto</Link>
 							</ul>
 						</nav>
 					</div>
