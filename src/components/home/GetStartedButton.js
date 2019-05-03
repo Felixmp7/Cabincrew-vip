8c4da3acffe08cce059c5e1cfb31c4690b7ac4be
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-03 08:48:46 -0400

    Configurando el scroll

--- src/components/home/GetStartedButton.js
+++ src/components/home/GetStartedButton.js
@@ -4,9 +4,16 @@ import { animateScroll as scroll} from 'react-scroll'
 //CSS
 import './../../styles/home/GetStarted/Button.css'
 
+const scrollType = {
+	duration: 1000,
+	delay: 50,
+	smooth: true, // linear “easeInQuint” “easeOutCubic”
+	offset: -10,
+}
+
 class GetStartedButton extends React.Component {
 	getStartedScroll = e => {
-		scroll.scrollTo(772)
+		scroll.scrollTo(772, scrollType)
 	}
 	render(){
 		return (
