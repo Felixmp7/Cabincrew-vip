513cd122f606a2dcfd2a0cf7330d82548c9b5b4e
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-03 09:11:17 -0400

    Añadiendo scroll inicial a cada componente

--- src/components/contacto/Contacto.js
+++ src/components/contacto/Contacto.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { animateScroll as scroll} from 'react-scroll'
 import '../../styles/contacto/Contacto.css'
 import Inac from '../../media/contacto/logotipo-inac.png'
 import Place from '../../media/contacto/place.png'
@@ -6,107 +7,121 @@ import Call from '../../media/contacto/call.png'
 import Mail from '../../media/contacto/mail.png'
 import Social from '../../media/contacto/social.png'
 
+const scrollType = {
+	duration: 300,
+	delay: 20,
+	smooth: true, // linear “easeInQuint” “easeOutCubic”
+	offset: -10,
+}
 
-const Contacto = () => (
-  <div className="Contacto">
-    <div className="Contacto-Header">
-      <div className="Title">
-        <h3>¿Cómo te podemos ayudar?</h3>
-      </div>
-      <div className="Subtitle">
-        <p>
-          Rellena el siguiente formulario para brindarte
-          la mejor solucion. <br/>
-          Estamos aquí para ti.
-        </p>
-      </div>
-      <div className="Form">
-        <form>
-          <div className="Especial">
-            <input className="Medio" type="text" placeholder="Nombre"/>
-            <input className="Medio" type="text" placeholder="Apellido" />
+class Contacto extends React.Component {
+  constructor(props){
+    super(props)
+    scroll.scrollToTop(scrollType)
+  }
+  render(){
+    return(
+      <div className="Contacto">
+        <div className="Contacto-Header">
+          <div className="Title">
+            <h3>¿Cómo te podemos ayudar?</h3>
           </div>
-          <input type="email" placeholder="Correo Electrónico" />
-          <input type="text" placeholder="Numero de Teléfono" />
-          <input type="text" placeholder="Escribe tu solicitud" />
-        </form>
-      </div>
-    </div>
-    <div className="Contacto-Info">
-      <div className="Izquierda">
-        <div className="Card">
-          <div className="Header">
-            <div className="Item">
-              <img src={Place} alt="icon1"/>
-            </div>
-            <div className="Item">
-              <p className="Azul">Oficina Principal</p>
-            </div>
+          <div className="Subtitle">
+            <p>
+              Rellena el siguiente formulario para brindarte
+              la mejor solucion. <br/>
+              Estamos aquí para ti.
+            </p>
           </div>
-          <p>
-            Urb. Colinas de los Caobos con Av. Boyacá,
-            Edif. Fundación La Salle, Oficina PB Oeste. Parroquia El Recreo,
-            Municipio Libertador, Distrito Capital 1050 Caracas - Venezuela
-          </p>
+          <div className="Form">
+            <form>
+              <div className="Especial">
+                <input className="Medio" type="text" placeholder="Nombre"/>
+                <input className="Medio" type="text" placeholder="Apellido" />
+              </div>
+              <input type="email" placeholder="Correo Electrónico" />
+              <input type="text" placeholder="Numero de Teléfono" />
+              <input type="text" placeholder="Escribe tu solicitud" />
+            </form>
+          </div>
         </div>
+        <div className="Contacto-Info">
+          <div className="Izquierda">
+            <div className="Card">
+              <div className="Header">
+                <div className="Item">
+                  <img src={Place} alt="icon1"/>
+                </div>
+                <div className="Item">
+                  <p className="Azul">Oficina Principal</p>
+                </div>
+              </div>
+              <p>
+                Urb. Colinas de los Caobos con Av. Boyacá,
+                Edif. Fundación La Salle, Oficina PB Oeste. Parroquia El Recreo,
+                Municipio Libertador, Distrito Capital 1050 Caracas - Venezuela
+              </p>
+            </div>
 
-        <div className="Card">
-          <div className="Header">
-            <div className="Item">
-              <img src={Call} alt="icon2"/>
+            <div className="Card">
+              <div className="Header">
+                <div className="Item">
+                  <img src={Call} alt="icon2"/>
+                </div>
+                <div className="Item">
+                  <p className="Verde">Llámanos</p>
+                </div>
+              </div>
+              <p>
+                +58(0212)625.9220 <br/>
+                +58(0212)418.2328
+              </p>
             </div>
-            <div className="Item">
-              <p className="Verde">Llámanos</p>
-            </div>
-          </div>
-          <p>
-            +58(0212)625.9220 <br/>
-            +58(0212)418.2328
-          </p>
-        </div>
 
-        <div className="Card">
-          <div className="Header">
-            <div className="Item">
-              <img src={Mail} alt="icon3"/>
+            <div className="Card">
+              <div className="Header">
+                <div className="Item">
+                  <img src={Mail} alt="icon3"/>
+                </div>
+                <div className="Item">
+                  <p className="Amarillo">Escribenos</p>
+                </div>
+              </div>
+              <p>info@cabincrewvip.com</p>
             </div>
-            <div className="Item">
-              <p className="Amarillo">Escribenos</p>
-            </div>
-          </div>
-          <p>info@cabincrewvip.com</p>
-        </div>
 
-      </div>
-      <div className="Derecha">
-        <div className="Mapa">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d980.7127179795509!2d-66.88379054062618!3d10.51240970370405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c2a593e97a4d761%3A0x875fc90b36d3fe1f!2sFundaci%C3%B3n+La+Salle+de+Ciencias+Naturales!5e0!3m2!1ses-419!2sve!4v1556389072077!5m2!1ses-419!2sve"
-            frameBorder="0"
-            allowFullScreen
-            title="Cabin"
-          >
-          </iframe>
-        </div>
-        <div className="Card">
-          <div className="Header">
-            <div className="Item">
-              <img src={Social} alt="icon4"/>
+          </div>
+          <div className="Derecha">
+            <div className="Mapa">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d980.7127179795509!2d-66.88379054062618!3d10.51240970370405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8c2a593e97a4d761%3A0x875fc90b36d3fe1f!2sFundaci%C3%B3n+La+Salle+de+Ciencias+Naturales!5e0!3m2!1ses-419!2sve!4v1556389072077!5m2!1ses-419!2sve"
+                frameBorder="0"
+                allowFullScreen
+                title="Cabin"
+              >
+              </iframe>
             </div>
-            <div className="Item">
-              <p className="Rojo">Redes Sociales</p>
+            <div className="Card">
+              <div className="Header">
+                <div className="Item">
+                  <img src={Social} alt="icon4"/>
+                </div>
+                <div className="Item">
+                  <p className="Rojo">Redes Sociales</p>
+                </div>
+              </div>
+              <p>Icons</p>
             </div>
           </div>
-          <p>Icons</p>
+        </div>
+        <div className="Contacto-Footer">
+          <div className="Image-Position">
+            <img src={Inac} alt="Logo Inac"/>
+          </div>
         </div>
       </div>
-    </div>
-    <div className="Contacto-Footer">
-      <div className="Image-Position">
-        <img src={Inac} alt="Logo Inac"/>
-      </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
 export default Contacto
--- src/components/servicios/Servicios.js
+++ src/components/servicios/Servicios.js
@@ -1,25 +1,53 @@
 import React from 'react'
+import { animateScroll as scroll} from 'react-scroll'
 import '../../styles/servicios/Servicios.css'
 import background from '../../media/servicio/estudiantes.jpg'
 import libros from '../../media/servicio/curso-libros.jpg'
 
-const Servicios = () => {
-  return (
-    <div className="Servicios">
-      <div className="Servicios-Header">
-        <h6 className="Title">Preparación de Calidad</h6>
-        <h3 className="Subtitle">
-          Curso Inicial para obtener la licencia de
-          Tripulante de Cabina
-        </h3>
-      </div>
-      <div className="Servicios-Image">
-        <img src={background} alt="bg"/>
-      </div>
-      <div className="Servicios-Contenido">
+const scrollType = {
+	duration: 300,
+	delay: 20,
+	smooth: true, // linear “easeInQuint” “easeOutCubic”
+	offset: -10,
+}
+
+class Servicios extends React.Component{
+  constructor(props){
+    super(props)
+    scroll.scrollToTop(scrollType)
+  }
+  render(){
+    return (
+      <div className="Servicios">
+        <div className="Servicios-Header">
+          <h6 className="Title">Preparación de Calidad</h6>
+          <h3 className="Subtitle">
+            Curso Inicial para obtener la licencia de
+            Tripulante de Cabina
+          </h3>
+        </div>
+        <div className="Servicios-Image">
+          <img src={background} alt="bg"/>
+        </div>
+        <div className="Servicios-Contenido">
+            <div className="Bloque">
+              <h4 className="Bloque-Title Rojo">
+                ¿Para quién está dirigido?
+              </h4>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Quis nesciunt eaque eligendi consectetur nulla dolorum quas,
+                id consequuntur laborum, aperiam! Dolorem ea error ipsa exercitationem amet,
+                id, sed suscipit officiis!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Quis nesciunt eaque eligendi consectetur nulla dolorum quas,
+                id consequuntur laborum, aperiam! Dolorem ea error ipsa exercitationem amet,
+                id, sed suscipit officiis!
+              </p>
+          </div>
           <div className="Bloque">
-            <h4 className="Bloque-Title Rojo">
-              ¿Para quién está dirigido?
+            <h4 className="Bloque-Title Verde">
+              Tiempo total de estudio
             </h4>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -31,43 +59,29 @@ const Servicios = () => {
               id consequuntur laborum, aperiam! Dolorem ea error ipsa exercitationem amet,
               id, sed suscipit officiis!
             </p>
-        </div>
-        <div className="Bloque">
-          <h4 className="Bloque-Title Verde">
-            Tiempo total de estudio
-          </h4>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Quis nesciunt eaque eligendi consectetur nulla dolorum quas,
-            id consequuntur laborum, aperiam! Dolorem ea error ipsa exercitationem amet,
-            id, sed suscipit officiis!
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Quis nesciunt eaque eligendi consectetur nulla dolorum quas,
-            id consequuntur laborum, aperiam! Dolorem ea error ipsa exercitationem amet,
-            id, sed suscipit officiis!
-          </p>
-        </div>
-        <div className="Bloque">
-          <h4 className="Bloque-Title Azul">
-            Requisitos
-          </h4>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Quis nesciunt eaque eligendi consectetur nulla dolorum quas,
-            id consequuntur laborum, aperiam! Dolorem ea error ipsa exercitationem amet,
-            id, sed suscipit officiis!
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Quis nesciunt eaque eligendi consectetur nulla dolorum quas,
-            id consequuntur laborum, aperiam! Dolorem ea error ipsa exercitationem amet,
-            id, sed suscipit officiis!
-          </p>
-        </div>
-        <div className="Bloque-Imagen">
-          <img src={libros} alt="curso-libros"/>
+          </div>
+          <div className="Bloque">
+            <h4 className="Bloque-Title Azul">
+              Requisitos
+            </h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Quis nesciunt eaque eligendi consectetur nulla dolorum quas,
+              id consequuntur laborum, aperiam! Dolorem ea error ipsa exercitationem amet,
+              id, sed suscipit officiis!
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Quis nesciunt eaque eligendi consectetur nulla dolorum quas,
+              id consequuntur laborum, aperiam! Dolorem ea error ipsa exercitationem amet,
+              id, sed suscipit officiis!
+            </p>
+          </div>
+          <div className="Bloque-Imagen">
+            <img src={libros} alt="curso-libros"/>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Servicios
