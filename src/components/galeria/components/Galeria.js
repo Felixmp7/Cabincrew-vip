b33950629b22b0deb090c16e7b182dc1b9c56e85
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-06-07 20:59:23 -0400

    Importando y Renderizando Banner

--- src/components/galeria/components/Galeria.js
+++ src/components/galeria/components/Galeria.js
@@ -1,20 +1,28 @@
 import React, {Component} from 'react'
 import {data} from '../../../galeria'
+import Banner from '../../banner/Banner'
 import '../../../styles/galeria/Galeria.css'
 
 class Galeria extends Component {
   render () {
     return (
       <div className="Container">
-        {
-          data.map((element,key) =>{
-            return(
-              <div key={key} className="Cont-Imagen">
-                <img src={element.image} className="Imagen" alt="Ejemplo" />
-              </div>
-            )
-          })
-        }
+        <Banner
+          title="Galeria de fotos"
+          contain="Contamos con un capital humano excepcional, certificados y
+          con mayor experiencia en el área de la tripulacion de cabina"
+        />
+        <div className="Container-Galery">
+          {
+            data.map((element,key) =>{
+              return(
+                <div key={key} className="Cont-Imagen">
+                  <img src={element.image} className="Imagen" alt="Ejemplo" />
+                </div>
+              )
+            })
+          }
+        </div>
       </div>
     )
   }
