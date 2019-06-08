43140e947847acc5655099958eb5e5cf2f446829
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-06-07 20:43:04 -0400

    Alineando imagenes

--- src/components/galeria/components/Galeria.js
+++ src/components/galeria/components/Galeria.js
@@ -1,17 +1,16 @@
 import React, {Component} from 'react'
 import {data} from '../../../galeria'
 import '../../../styles/galeria/Galeria.css'
-console.log(data);
+
 class Galeria extends Component {
   render () {
     return (
       <div className="Container">
         {
           data.map((element,key) =>{
-            console.log(element);
             return(
               <div key={key} className="Cont-Imagen">
-                <img src={element.image} className="Imagen" />
+                <img src={element.image} className="Imagen" alt="Ejemplo" />
               </div>
             )
           })
